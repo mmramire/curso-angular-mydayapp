7a80e4ac96c894750124ab1ffdd178a3d79ca7e5
mmramire <huo_mmram@hotmail.com>
2024-02-29 12:41:06 -0300

    Hasta firebase

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
+import {
+  Component,
+  Injector,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -9,9 +16,13 @@ import { Task } from './../../models/task.model';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  // Reactividad con signals
+  // computejd calcula nuevos estados y genera nueva señal a partir de esoss estados
+  // effect nos sirve es para trackear o espiar cada vez que una señal se emita o cambie
+  // signal
 
   tasks = signal<Task[]>([]);
   filter = signal<'all' | 'pending' | 'completed'>('all');
@@ -19,21 +30,20 @@ export class HomeComponent {
     const filter = this.filter();
     const tasks = this.tasks();
     if (filter === 'pending') {
-      return tasks.filter(task => !task.completed);
+      return tasks.filter((task) => !task.completed);
     }
     if (filter === 'completed') {
-      return tasks.filter(task => task.completed);
+      return tasks.filter((task) => task.completed);
     }
     return tasks;
-  })
+  });
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
-    validators: [  
-      Validators.required,
-    ]
+    validators: [Validators.required],
   });
 
+  //porque tenemos el effect fuera del constructor
   injector = inject(Injector);
 
   ngOnInit() {
@@ -46,10 +56,13 @@ export class HomeComponent {
   }
 
   trackTasks() {
-    effect(() => {
-      const tasks = this.tasks();
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-    }, { injector: this.injector });
+    effect(
+      () => {
+        const tasks = this.tasks();
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      },
+      { injector: this.injector }
+    );
   }
 
   changeHandler() {
@@ -66,59 +79,61 @@ export class HomeComponent {
     const newTask = {
       id: Date.now(),
       title,
-      completed: false
-    }
-    this.tasks.update(prevState => [...prevState, newTask]);
+      completed: false,
+    };
+    this.tasks.update((prevState) => [...prevState, newTask]);
   }
-  
+
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
   }
 
   updateTask(index: number) {
-    this.tasks.update(prevState => {
+    this.tasks.update((prevState) => {
       return prevState.map((task, position) => {
         if (position === index) {
           return {
             ...task,
-            completed: !task.completed
-          }
+            completed: !task.completed,
+          };
         }
         return task;
-      })
+      });
     });
   }
 
   updateTaskEditingMode(index: number) {
-    this.tasks.update(prevState => {
+    this.tasks.update((prevState) => {
       return prevState.map((task, position) => {
         if (position === index) {
           return {
             ...task,
-            editing: true
-          }
+            editing: true,
+          };
         }
         return {
           ...task,
-          editing: false
+          editing: false,
         };
-      })
+      });
     });
   }
 
   updateTaskText(index: number, event: Event) {
     const input = event.target as HTMLInputElement;
-    this.tasks.update(prevState => {
+    this.tasks.update((prevState) => {
       return prevState.map((task, position) => {
         if (position === index) {
           return {
             ...task,
             title: input.value,
-            editing: false
-          }
+            editing: false,
+          };
         }
         return task;
-      })
+      });
     });
   }
 
@@ -126,6 +141,3 @@ export class HomeComponent {
     this.filter.set(filter);
   }
 }
-
-
-
